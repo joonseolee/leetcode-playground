6832ee2917629e413d0e1eca9b52386f868c6efd
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-01-04 19:31:56 +0900

    Time: 2 ms (95.87%), Space: 45 MB (38.72%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,61 +2,30 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Set<Combination> result = new HashSet();
-        logic(result, candidates, new ArrayList<>(), 0, target);
-        return result.stream()
-            .map(Combination::getValues)
-            .collect(Collectors.toList());
+        List<List<Integer>> result = new ArrayList();
+        combinations(candidates, target, result, 0, new ArrayList<>());
+        return result;
     }
     
-    private void logic(Set<Combination> result,
-                       int[] candidates,
-                       List<Integer> values,
-                       int valueSum,
-                       int target) {
-        if (valueSum == target) {
-            result.add(new Combination(values));
+    private void combinations(int[] candidates,
+                              int target,
+                              List<List<Integer>> result, 
+                              int index, 
+                              List<Integer> values) {
+        if (index == candidates.length) {
+            if (target == 0) {
+                result.add(new ArrayList<>(values));
+            }
+            
             return;
         }
-
-        for (int candidate : candidates) {
-            int tempSum = valueSum + candidate;
-            if (tempSum <= target) {
-                List<Integer> recentValues = new ArrayList<>(values);
-                recentValues.add(candidate);
-                logic(result, candidates, recentValues, tempSum, target);
-            }
-        }
-    }
-
-    private static class Combination {
-        private List<Integer> values;
         
-        public Combination(List<Integer> numbers) {
-            this.values = numbers.stream().sorted().collect(Collectors.toList());
+        if (candidates[index] <= target) {
+            values.add(candidates[index]);
+            combinations(candidates, target - candidates[index], result, index, values);
+            values.remove(values.size() - 1);
         }
         
-        public List<Integer> getValues() {
-            return this.values;
-        }
-        
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(this.values);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (o == null) {
-                return false;
-            }
-
-            if (o instanceof Combination) {
-                Combination other = (Combination) o;
-                return this.hashCode() == other.hashCode();
-            }
-
-            return false;
-        }
+        combinations(candidates, target, result, index + 1, values);
     }
 }
