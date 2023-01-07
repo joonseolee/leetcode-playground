75c40d86343b1f6b2003630ff44ac5a20494b2e5
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-01-07 10:37:09 +0900

    Time: 4 ms (94.72%), Space: 42 MB (94.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,16 +1,37 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] map = new int[26];
-        for(char c : s1.toCharArray()) map[c - 'a']++;
-        int j = 0, i = 0;
-        int count_chars = s1.length();
-        while(j < s2.length()){
-            if(map[s2.charAt(j++) - 'a']-- > 0)
-                count_chars--;
-            if(count_chars == 0) return true;
-            if(j - i == s1.length() && map[s2.charAt(i++) - 'a']++ >= 0)
-                count_chars++;
+        if (s1.length() > s2.length()) {
+            return false;
         }
+        
+        int[] wordLocations = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            wordLocations[s1.charAt(i) - 'a']++;
+            wordLocations[s2.charAt(i) - 'a']--;
+        }
+        
+        if (isZero(wordLocations)) {
+            return true;
+        }
+        
+        for (int i = s1.length(); i < s2.length(); i++) {
+            wordLocations[s2.charAt(i) - 'a']--;
+            wordLocations[s2.charAt(i - s1.length()) - 'a']++;
+            if (isZero(wordLocations)) {
+                return true;
+            }
+        }
+        
         return false;
+    }
+    
+    public boolean isZero(int[] wordLocations) {
+        for (int value : wordLocations) {
+            if (value != 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
