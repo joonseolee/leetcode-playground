ab37a91ab0fe16899625187df394ebd9d3d1ccc5
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-01-29 16:06:40 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (81.93%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int left = 0;
+        int mid = 0;
+        int right = nums.length -1;
+
+        while(mid<=right){
+            if(nums[mid] == 0) {
+                swap(nums, mid, left);
+                left++;
+                mid++;
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else if(nums[mid] == 2) {
+                swap(nums, mid, right);
+                right--;
+            }
+        }
+    }
+    
+    public void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
