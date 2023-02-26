a1df4b5a3d65369b170d64d9b997c8bbc4cd88b6
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-02-26 22:17:54 +0900

    Time: 17 ms (24.11%), Space: 45.1 MB (34.50%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -5,18 +5,11 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        List<Integer> list = map.entrySet().stream().sorted(new Comparator<Map.Entry<Integer, Integer>>() {
+        return map.entrySet().stream().sorted(new Comparator<Map.Entry<Integer, Integer>>() {
 			@Override
 			public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
 				return Integer.compare(o2.getValue(), o1.getValue());
 			}
-		}).map(Map.Entry::getKey).collect(Collectors.toList());
-        
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = list.get(i);
-        }
-        
-        return result;
+		}).limit(k).mapToInt(it -> it.getKey()).toArray();
     }
 }
