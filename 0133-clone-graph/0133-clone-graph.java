9b44df94299b89ffd021cf4270eb183aff111de1
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-03-05 11:14:26 +0900

    Time: 25 ms (94.80%), Space: 43.4 MB (13.03%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -23,35 +23,59 @@ class Solution {
         if (node == null) {
             return null;
         }
-        Map<Integer, Node> map = new HashMap<>();
-        fillUniqueMap(node, map);
-        addNeighbours(node, map, new HashSet<>());
-        return map.get(node.val);
+        Node copy = new Node(node.val);
+        Node[] visited = new Node[101];
+        dfs(node , copy , visited);
+        return copy;
     }
-
-
-    public void fillUniqueMap(Node node, Map<Integer, Node> map) {
-        if (node == null || map.containsKey(node.val)) {
-            return;
+    
+    public void dfs(Node node , Node copy , Node[] visited) {
+        visited[copy.val] = copy;
+        for (Node n : node.neighbors) {
+            if (visited[n.val] == null) {
+                Node newNode = new Node(n.val);
+                copy.neighbors.add(newNode);
+                dfs(n , newNode , visited);
+            } else {
+                copy.neighbors.add(visited[n.val]);
+            }
         }
+    }
+    
+    
+//     public Node cloneGraph(Node node) {
+//         if (node == null) {
+//             return null;
+//         }
+//         Map<Integer, Node> map = new HashMap<>();
+//         fillUniqueMap(node, map);
+//         addNeighbours(node, map, new HashSet<>());
+//         return map.get(node.val);
+//     }
+
+
+//     public void fillUniqueMap(Node node, Map<Integer, Node> map) {
+//         if (node == null || map.containsKey(node.val)) {
+//             return;
+//         }
         
-        map.put(node.val, new Node(node.val));
-        for (Node one : node.neighbors) {
-            fillUniqueMap(one, map);
-        }
-    }
+//         map.put(node.val, new Node(node.val));
+//         for (Node one : node.neighbors) {
+//             fillUniqueMap(one, map);
+//         }
+//     }
 
 
-    public void addNeighbours(Node node, Map<Integer, Node> map, Set<Integer> visited) {
-        if (node == null || visited.contains(node.val)) {
-            return;
-        }
+//     public void addNeighbours(Node node, Map<Integer, Node> map, Set<Integer> visited) {
+//         if (node == null || visited.contains(node.val)) {
+//             return;
+//         }
         
-        visited.add(node.val);
-        Node cloneNode = map.get(node.val);
-        for (Node one : node.neighbors) {
-            cloneNode.neighbors.add(map.get(one.val));
-            addNeighbours(one, map, visited);
-        }
-    }
+//         visited.add(node.val);
+//         Node cloneNode = map.get(node.val);
+//         for (Node one : node.neighbors) {
+//             cloneNode.neighbors.add(map.get(one.val));
+//             addNeighbours(one, map, visited);
+//         }
+//     }
 }
