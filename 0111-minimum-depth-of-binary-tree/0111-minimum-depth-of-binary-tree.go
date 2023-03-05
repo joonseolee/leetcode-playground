07fb85a9f90f9cd63ddb805e1ac0cabda8bb9479
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-03-05 12:01:56 +0900

    Time: 230 ms (20.71%), Space: 26.6 MB (5.30%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.go
@@ -0,0 +1,27 @@
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+    if root == nil {
+		return 0
+	}
+
+	if root.Left == nil {
+		return 1 + minDepth(root.Right)
+	}
+
+	if root.Right == nil {
+		return 1 + minDepth(root.Left)
+	}
+
+	value := math.Min(float64(minDepth(root.Left)), float64(minDepth(root.Right)))
+
+	return 1 + int(value)
+}
