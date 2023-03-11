a5d88b998c2610f59d805e3a32734f939d3a6761
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-03-12 08:57:27 +0900

    Time: 1125 ms (85.93%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT a.id
+    FROM (
+        SELECT MIN(id) id
+        FROM Person
+        GROUP BY email
+    ) a
+)
