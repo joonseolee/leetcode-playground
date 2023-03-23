f62339d0c90b0c8a7e1b0d9177f72274199aa407
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-03-23 23:05:24 +0900

    Time: 7 ms (44.66%), Space: 53.9 MB (60.89%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -4,11 +4,13 @@ class Solution {
             return 0;
         }
 
-        int max = 0, n = matrix.length, m = matrix[0].length;
-        int[][] dp = new int[n + 1][m + 1];
+        int max = 0;
+        int maxHeight = matrix.length;
+        int minHeight = matrix[0].length;
+        int[][] dp = new int[maxHeight + 1][minHeight + 1];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+        for (int i = 1; i <= maxHeight; i++) {
+            for (int j = 1; j <= minHeight; j++) {
                 if (matrix[i - 1][j - 1] == '1') {
                     dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                     max = Math.max(max, dp[i][j]);
