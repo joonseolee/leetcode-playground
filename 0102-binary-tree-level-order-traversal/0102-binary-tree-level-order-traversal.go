2aad0495fc3a869f83e8e5b8794608f4ca6da802
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2023-04-05 23:22:14 +0900

    Time: 1 ms (61.03%), Space: 3.1 MB (22.90%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.go
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.go
@@ -1,39 +1,27 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
  * }
-          1
-        2.  3
-      4. n n 5
  */
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        recursive(result, root, 0);
-        return result;
+func levelOrder(root *TreeNode) [][]int {
+    var result [][]int
+    recursive(&result, root, 0)
+    return result
+}
+
+func recursive(result *[][]int, root *TreeNode, level int) {
+    if root == nil {
+        return
     }
     
-    public void recursive(List<List<Integer>> result, TreeNode root, int level) {
-        if(root == null) {
-            return;
-        }
-            
-        if(result.size() < level + 1) {
-            result.add(new ArrayList<>());
-        }
-
-        result.get(level).add(root.val);
-        recursive(result, root.left, level + 1);
-        recursive(result, root.right, level + 1);
+    if len(*result) < level + 1 {
+        *result = append(*result, []int{})
     }
+    
+    (*result)[level] = append((*result)[level], root.Val)
+    recursive(result, root.Left, level + 1)
+    recursive(result, root.Right, level + 1)
 }
