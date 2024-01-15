a4e70a219cbd5af009ecbd24ef0c3439bc29442d
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2024-01-15 15:38:49 +0900

    Time: 2 ms (89.01%), Space: 44.7 MB (36.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (target == nums[i] + nums[j]) {
-                    return new int[]{i, j};
-                }
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
             }
+            
+            map.put(target - nums[i], i);
         }
         
         return new int[2];
