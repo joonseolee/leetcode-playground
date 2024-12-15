b7a3740ae6d44090f5c4ca3d8e18a2d553f60307
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2024-12-15 17:36:56 +0900

    Time: 15 ms (18.94%) | Memory: 45.6 MB (24.21%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -4,56 +4,29 @@ class Solution {
             return s;
         }
 
-        // 2차원 배열 생성
-        Character[][] matrix = new Character[numRows][s.length()];
+        int index = 0;
+        int arg = 1;
 
-        // 2차원 배열내에 필드 초기화
-        initMatrix(matrix, s, 0, 0, 0, true);
-
-        // 2차원 배열에서 문자열 리턴
-        return acquireValue(matrix);
-    }
-
-    private void initMatrix(Character[][] matrix, String value, int valueIndex, int i, int j, boolean isDown) {
-        int valueSize = value.length();
-        if (valueIndex >= valueSize) {
-            return;
+        List<String> values = new ArrayList<>();
+        for (int i = 0; i < numRows ; i++) {
+            values.add("");
         }
 
-        int iSize = matrix.length;
+        for (char character : s.toCharArray()) {
+            String element = values.get(index);
+            values.set(index, element + character);
 
-        if (i < iSize && isDown) {
-            matrix[i][j] = value.charAt(valueIndex);
-            initMatrix(matrix, value, valueIndex + 1, i + 1, j, true);
-            return;
-        }
-
-        if (i == iSize && isDown) {
-            initMatrix(matrix, value, valueIndex, i - 2, j + 1, false);
-            return;
-        }
-
-        if (i > -1 && !isDown) {
-            matrix[i][j] = value.charAt(valueIndex);
-            initMatrix(matrix, value, valueIndex + 1, i - 1, j + 1, false);
-            return;
-        }
-
-        if (i == -1) {
-            initMatrix(matrix, value, valueIndex, 1, j - 1, true);
-        }
-    }
-
-    private String acquireValue(Character[][] matrix) {
-        StringBuilder result = new StringBuilder();
-        for (Character[] row : matrix) {
-            for (Character ch : row) {
-                if (ch != null) {
-                    result.append(ch);
-                }
+            if (index == 0) {
+                arg = 1;
             }
+
+            if (index == numRows - 1) {
+                arg = -1;
+            }
+
+            index += arg;
         }
 
-        return result.toString();
+        return String.join("", values);
     }
 }
