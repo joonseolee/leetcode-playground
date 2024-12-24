fce7bcfdace7abecfca7b9c9da9a3042b92274f5
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2024-12-24 12:49:41 +0900

    Time: 8 ms (52.22%) | Memory: 42.1 MB (59.78%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -4,25 +4,32 @@ class Solution {
             return 0;
         }
         
-        char[] balloons = "balloon".toCharArray();
         Map<Character, Integer> map = new HashMap<>();
-        for (char value : text.toCharArray()) {
-            map.put(value, map.getOrDefault(value, 0) + 1);
-        }
-        
-        int result = 0;
-        
-        while (true) {
-            for (char balloon : balloons) {
-                Integer count = map.getOrDefault(balloon, 0);
-                if (count == 0) {
-                    return result;
-                }
+        map.put('b', 0);
+        map.put('a', 0);
+        map.put('l', 0);
+        map.put('o', 0);
+        map.put('n', 0);
 
-                map.put(balloon, count - 1);
+        for (char c : text.toCharArray()) {
+            int count = map.getOrDefault(c, -1);
+            if (count > -1) {
+                map.put(c, count + 1);
             }
-
-            result++;
         }
+
+        map.put('l', map.get('l') / 2);
+        map.put('o', map.get('o') / 2);
+
+        int min = Integer.MAX_VALUE;
+        for (Integer value : map.values()) {
+            if (value == 0) {
+                return 0;
+            }
+            
+            min = Math.min(min, value);
+        }
+
+        return min;
     }
 }
