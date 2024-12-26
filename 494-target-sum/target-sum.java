69c700639f090e0c1bfb5491b494d3ee222a647b
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2024-12-26 17:09:09 +0900

    Time: 101 ms (46.83%) | Memory: 45.9 MB (5.48%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,4 +1,8 @@
+import java.util.HashMap;
+
 class Solution {
+    private HashMap<String, Integer> memo = new HashMap<>();
+
     public int findTargetSumWays(int[] nums, int target) {
         return find(nums, target, 0, 0);
     }
@@ -12,9 +16,18 @@ class Solution {
             return 0;
         }
 
+        String key = index + "," + sum;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        // 두 가지 경우의 수를 계산
         int a = find(nums, target, index + 1, sum + nums[index]);
         int b = find(nums, target, index + 1, sum - nums[index]);
 
-        return a + b;
+        // 결과를 저장하고 리턴
+        int result = a + b;
+        memo.put(key, result);
+        return result;
     }
 }
