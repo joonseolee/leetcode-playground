1599f883f1ed56dcfc27f341aeeb1b920155336c
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2025-02-14 17:31:05 +0900

    Time: 18 ms (88.87%) | Memory: 42.1 MB (95.91%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,29 +1,41 @@
 class Solution {
-    public int minimumDeleteSum(String aText, String bText) {
-        int aTextLength = aText.length();
-        int bTextLength = bText.length();
-        int[][] dp = new int[aTextLength + 1][bTextLength + 1];
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        
+        // 1D DP 배열 사용 (이전 줄을 저장)
+        int[] prev = new int[n + 1];
 
-        for (int i = 1; i <= aTextLength; i++) {
-            dp[i][0] = dp[i - 1][0] + aText.charAt(i - 1);
+        // 첫 번째 문자열이 비어있을 때 초기화
+        for (int j = 1; j <= n; j++) {
+            prev[j] = prev[j - 1] + s2.charAt(j - 1);
         }
 
-        for (int j = 1; j <= bTextLength; j++) {
-            dp[0][j] = dp[0][j - 1] + bText.charAt(j - 1);
-        }
+        // 현재 행을 저장할 배열
+        int[] curr = new int[n + 1];
 
-        for (int i = 1; i <= aTextLength; i++) {
-            for (int j = 1; j <= bTextLength; j++) {
-                if (aText.charAt(i - 1) == bText.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
+        // DP 테이블 채우기
+        for (int i = 1; i <= m; i++) {
+            // 첫 번째 열 (s2가 비어 있는 경우)
+            curr[0] = prev[0] + s1.charAt(i - 1);
+
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    curr[j] = prev[j - 1]; // 같은 문자는 삭제할 필요 없음
                 } else {
-                    dp[i][j] = Math.min(
-                            dp[i - 1][j] + aText.charAt(i - 1),
-                            dp[i][j - 1] + bText.charAt(j - 1));
+                    curr[j] = Math.min(
+                        prev[j] + s1.charAt(i - 1), // s1[i-1]을 삭제
+                        curr[j - 1] + s2.charAt(j - 1) // s2[j-1]을 삭제
+                    );
                 }
             }
+
+            // 이전 줄을 현재 줄로 업데이트
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
         }
 
-        return dp[aTextLength][bTextLength];
+        return prev[n];
     }
 }
