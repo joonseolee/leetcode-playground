7331c011eb7e9d3742959b8682bd063b1bb9ea73
Joonseo Lee <43516757+joonseolee@users.noreply.github.com>
2025-02-23 22:00:31 +0900

    Time: 38 ms (83.66%) | Memory: 58 MB (29.04%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.java
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.java
@@ -1,10 +1,9 @@
 class Solution {
     public int longestSubsequence(int[] arr, int difference) {
-        HashMap<Integer, Integer> dp = new HashMap<>();
+        Map<Integer, Integer> dp = new HashMap<>();
         int result = 0;
         
         for (int num : arr) {
-            // 현재 num을 이어 붙일 수 있는 subsequence의 길이를 계산
             int count = dp.getOrDefault(num - difference, 0) + 1;
             dp.put(num, count);
             result = Math.max(result, count);
